2978ef493e94b2b7c340109e12c05918ecd4b3d2
Dev <83149395+dev-odf@users.noreply.github.com>
2025-11-25 18:28:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+import INDEX_HTML from './index.html';
+import ICON from './icon-nobg.png';
+
 export default {
   async fetch(request) {
+    const { pathname } = new URL(request.url);
     if (request.method !== "GET") {
       return new Response("Method Not Allowed", { status: 405 });
     }
-    const html = await INDEX_HTML;
-    return new Response(html, {
-      headers: {
-        "content-type": "text/html; charset=UTF-8"
-      }
-    });
+ 
+    if (pathname === "/icon-nobg.png" || pathname === "/src/icon-nobg.png") {
+      return new Response(ICON, {
+        headers: { "content-type": "image/png" }
+      });
+    }
+ 
+    if (pathname === "/" || pathname === "/index.html") {
+      return new Response(INDEX_HTML, {
+        headers: { "content-type": "text/html; charset=UTF-8" }
+      });
+    }
+ 
+    return new Response("Not Found", { status: 404 });
   }
 };
- 
-import INDEX_HTML from './index.html';
